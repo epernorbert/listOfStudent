56ca10e8fc6790ff30c524a430db2d194799d650
epernorbert <eper.norbert98@gmail.com>
2024-11-16 12:45:21 +0100

    Init project and init database

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SQLiteProvider, useSQLiteContext } from "expo-sqlite";
+
+// Initalize the database
+async function initialiseDatabase(db) {
+  try {
+    await db.execAsync(`
+      PRAGMA journal_mode = WAL;
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        firstname TEXT,
+        lastname TEXT,
+        age INTEGER,
+        email TEXT
+      );
+    `);
+    console.log('Database initialised');
+  } catch (error) {
+    console.log("Error while initializin the database : ", error);
+  }
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SQLiteProvider databaseName="example.db" onInit={initialiseDatabase}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SQLiteProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
